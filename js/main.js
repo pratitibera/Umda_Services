cd9bbadd6409159d22e7c1b47963bddc6bbda528
Pratiti Bera <remabimal0801@gmail.com>
2022-01-28 14:03:02 +0530

    -

--- js/main.js
+++ js/main.js
@@ -11,15 +11,15 @@ menuBtn.addEventListener('click', () => {
     }
 });
 
-$("#desktop_work > div > a").click(function(){
-  $(".worksection").addClass("d-none");
-  var id = $(this).attr('href');
-  $(id).removeClass("d-none");
+$("#desktop_work > div > a").click(function () {
+    $(".worksection").addClass("d-none");
+    var id = $(this).attr('href');
+    $(id).removeClass("d-none");
 });
-$("#mobile_work > li > a").click(function(){
-  $(".worksection").addClass("d-none");
-  var id = $(this).attr('href');
-  $(id).removeClass("d-none");
+$("#mobile_work > li > a").click(function () {
+    $(".worksection").addClass("d-none");
+    var id = $(this).attr('href');
+    $(id).removeClass("d-none");
 });
 jQuery("#backtotop").click(function () {
     jQuery("body,html").animate({
@@ -124,18 +124,50 @@ $(".about_topics > li").click(function () {
 });
 
 
-function prevBlog(){
+function prevBlog() {
     v = parseInt($("#blogSlider input[name='blogSlider']:checked").val()) - 1;
-    if(v < 1){
-      v = 5;
+    if (v < 1) {
+        v = 5;
     }
     document.getElementById('s' + v).checked = true;
-  }
+}
 
-  function nextBlog(){
+function nextBlog() {
     v = parseInt($("#blogSlider input[name='blogSlider']:checked").val()) + 1;
-    if(v > 5){
-      v = 1;
+    if (v > 5) {
+        v = 1;
     }
     document.getElementById('s' + v).checked = true;
-  }
+}
+
+
+//selecting all required elements
+if (screen.width > 500) {
+    var filterItem = document.querySelector(".itemsdesktop");
+    var filterImg = document.querySelectorAll(".gallery .image");
+} else {
+    var filterItem = document.querySelector(".itemsmob");
+    var filterImg = document.querySelectorAll(".gallery .image");
+}
+
+window.onload = () => { //after window loaded
+    filterItem.onclick = (selectedItem) => { //if user click on filterItem div
+        if (selectedItem.target.classList.contains("item")) { //if user selected item has .item class
+            filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
+            selectedItem.target.classList.add("active"); //add that active class on user selected item
+            let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of user selected item and store in a filtername variable
+            filterImg.forEach((image) => {
+                let filterImges = image.getAttribute("data-name"); //getting image data-name value
+                //if user selected item data-name value is equal to images data-name value
+                //or user selected item data-name value is equal to "all"
+                if ((filterImges == filterName) || (filterName == "all")) {
+                    image.classList.remove("hide"); //first remove the hide class from the image
+                    image.classList.add("show"); //add show class in image
+                } else {
+                    image.classList.add("hide"); //add hide class in image
+                    image.classList.remove("show"); //remove show class from the image
+                }
+            });
+        }
+    }
+}
